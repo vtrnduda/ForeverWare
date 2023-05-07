d5f57bcd34abebe640cfaacc4ebff5e370ac6906
vtrnduda <vtrnduda@gmail.com>
2023-05-07 01:17:33 -0300

    update responsive

--- js/rewards.js
+++ js/rewards.js
@@ -6,19 +6,24 @@ export const rewards = [
           price:  45000,
         },
       {
-        img: './assets/images/rewards1.png',
-          title: 'Iphone',
-          price:  45000,
+        img: './assets/images/rewards2.png',
+          title: 'Capa',
+          price:  10000,
         }, 
       {
-        img: './assets/images/rewards1.png',
-          title: 'Iphone',
+        img: './assets/images/rewards3.png',
+          title: '10% off',
+          price:  350,
+        },
+      {
+        img: './assets/images/rewards4.png',
+          title: 'Carregador portátil',
           price:  45000,
         },
       {
         img: './assets/images/rewards1.png',
           title: 'Iphone',
           price:  45000,
-        }
+        },
  
    ]
